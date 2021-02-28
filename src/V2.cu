51ba7212d32be3d534cf17ee58659c72ef47204a
AntoniosOurdas <45215986+AntoniosOurdas@users.noreply.github.com>
2021-02-28 23:28:33 +0200

    Updated V2

--- src/V2.cu
+++ src/V2.cu
@@ -1,10 +1,22 @@
 #include "utilities.cuh"
 
 // CUDA kernel to compute denoise image
-__global__ void nonLocalMeans(double* P, int m, int n, int w, double filtSigma, double* F) {
+__global__ void nonLocalMeans(double* X, int m, int n, int w, double filtSigma, double* F, double* W) {
 
-  __shared__ int size = (1024+(w-1))(1024+(w-1));
-  __shared__ double localImageBlock[size];
+  // At most (16+(7-1))x(16+(7-1))
+  __shared__ double localImageBlock[484];
+  __shared__ int index;
+  __shared__ double K[25];
+
+  // Load kenrel once in shared memory
+  if(threadIdx.x == 0) {
+    for(int i = 0; i < 5; ++i)
+      for(int j = 0; j < 5; ++j)
+        K[i*5+j] = W[i*5+j];
+  }
+  // Other threads wait for the kernel to be loaded
+  __syncthreads();
+
   double Wxy = 0.0;
   double Zx = 0.0;
   double D = 0.0;
@@ -12,27 +24,50 @@ __global__ void nonLocalMeans(double* P, int m, int n, int w, double filtSigma, 
   int i = blockIdx.x;
   int j = threadIdx.x;
 
-  F[i*n+j] = 0.0;
+  F[i*blockDim.x+j] = 0.0;
   Zx = 0.0;
-  for(int k = 0; k < m; ++k) {
-    for(int l = 0; l < n; ++l) {
-        Wxy = 0.0;
-        D = 0.0;
-        for(int p = -(w-1)/2; p <= (w-1)/2; ++p) {
-          for(int q = -(w-1)/2; q <= (w-1)/2; ++q) {
-            // D += pow(P[(i+p)*(n+w-1)+(j+q)] - P[(k+p)*(n+w-1)+(l+q)], 2.0);
-            D += pow((P[i*n*w*w+j*w*w+(p+(w-1)/2)*w+(q+(w-1)/2)] - P[k*n*w*w+l*w*w+(p+(w-1)/2)*w+(q+(w-1)/2)]), 2.0);
 
-          }
+
+  // Iterate all pixel blocks (m*n/(16*16) in size)
+  for(int blockNoX = 0; blockNoX < m/16; ++blockNoX) {
+    for(int blockNoY = 0; blockNoY < n/16; ++blockNoY) {
+      // Only one thread will transfer data to shared memory
+      if(j == 0) {
+        index = 0;
+        for(int k = (blockNoX*16); k < (blockNoX*16)+(16+(w-1)); ++k) {
+        	for(int l = (blockNoY*16); l < (blockNoY*16)+(16+(w-1)); ++l) {
+            localImageBlock[index] = X[k*(n+w-1)+l];
+            ++index;
+        	}
+        	printf("\n");
         }
-        Wxy = exp(-D/(filtSigma*filtSigma));
-        Zx += Wxy;
-        // P[k][l][(w-1)/2][(w-1)/2] is the center pixel of current patch
-        F[i*n+j] += Wxy * P[k*n*w*w + l*w*w + (w-1)/2*w + (w-1)/2];
+      }
+      // Other threads wait for the pixels
+      // to be transferred to shared memory
+      __syncthreads();
+
+      // Now find weights for the pixels loaded from global memory
+      for(int k = (w-1)/2; k < 16+(w-1)/2; ++k) {
+        for(int l = (w-1)/2; l < 16+(w-1)/2; ++l) {
+            Wxy = 0.0;
+            D = 0.0;
+            for(int p = -(w-1)/2; p <= (w-1)/2; ++p) {
+              for(int q = -(w-1)/2; q <= (w-1)/2; ++q) {
+                int temp = ((X[(i+p)*(n+w-1)+(j+q)] - localImageBlock[(k+p)*(n+w-1)+(l+q)]));
+                D += K[(p+(w-1)/2)*w+(q+(w-1)/2)]*temp*temp;
+              }
+            }
+            Wxy = exp(-D/(filtSigma*filtSigma));
+            Zx += Wxy;
+
+            F[i*n+j] += Wxy * X[k*(n+w-1)+l];
+        }
+      }
+      // Continue with next block of pixels
     }
   }
-  F[i*n+j] /= Zx;
 
+  F[i*n+j] /= Zx;
   return;
 }
 
@@ -56,7 +91,7 @@ int main(int argc, char* argv[]) {
   }
 
   if(m != 64 && m != 128 && m != 256) {
-    printf("Only 64x64, 128x128 and 256x256 size images supported\n");
+    printf("Only 64x64, 128x128 and 256x256 image sizes supported\n");
     return 1;
   }
 
@@ -89,20 +124,22 @@ int main(int argc, char* argv[]) {
 
   // Original Image extended to fit patches on the edges [(m+w-1)-by-(n+w-1)]
   double* X = (double*)malloc((m+w-1)*(n+w-1)*sizeof(double));
-  // 3D Patch Cube [m-by-n-by-w-by-w]
-  double* P = (double*)malloc(m*n*w*w*sizeof(double));
   // Filtered image [m-by-n]
   double* F = (double*)malloc(m*n*sizeof(double));
   // Residual image (F - x)
   double* R = (double*)malloc(m*n*sizeof(double));
 
   // 3D Patch cude pointer for GPU memory
-  double* deviceP = NULL;
-  cudaMalloc(&deviceP, m*n*w*w*sizeof(double));
+  double* deviceX = NULL;
+  cudaMalloc(&deviceX, m*n*sizeof(double));
 
   // Filtered image pointer for GPU memory
   double* deviceF = NULL;
   cudaMalloc(&deviceF, m*n*sizeof(double));
+
+  // Gaussian kernel
+  double* deviceW = NULL;
+  cudaMalloc(&deviceW, w*w*sizeof(double));
 
   FILE* fptr = fopen(argv[6], "r");
   for(int i = (w-1)/2; i < m+(w-1)/2; ++i)
@@ -146,33 +183,18 @@ int main(int argc, char* argv[]) {
 
   }
 
-  // Calculate all w-by-w patches from X multiplied
-  // with gaussian kernel and save them to P
-  // (i,j) is the center pixel of each patch
-  // (k,l) is the patch element
-  // appropriate offsets are used
-  for(int i = (w-1)/2; i < m+(w-1)/2;++i) {
-      for(int j = (w-1)/2; j < n+(w-1)/2; ++j) {
-        for(int k = -(w-1)/2; k <= (w-1)/2; ++k) {
-            for(int l = -(w-1)/2; l <= (w-1)/2; ++l) {
-              P[(i-(w-1)/2)*n*w*w+(j-(w-1)/2)*w*w+(k+(w-1)/2)*w+(l+(w-1)/2)] =
-              X[(i+k)*(n+w-1)+(j+l)]*W[(k+(w-1)/2)*w+(l+(w-1)/2)];
-            }
-        }
-      }
-  }
-
   // Write noisy image to csv txt file
   // used by matlab script
   char outputFileName[100] = "";
-  sprintf(outputFileName, "../output_images/output_images_csv_txt/output_images_V1/%s_%d_%d_noisy.txt", argv[7], n, w);
+  sprintf(outputFileName, "../output_images/output_images_csv_txt/output_images_V2/%s_%d_%d_noisy.txt", argv[7], n, w);
   // printf("Writing noisy image to %s\n", outputFileName);
   printMatrixCsv(X, m+w-1, n+w-1, outputFileName);
 
   // Copy data for input and output
   // from CPU memory to GPU memory
-  cudaMemcpy(deviceP, P, m*n*w*w*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceX, X, (m+w-1)*(n+w-1)*sizeof(double), cudaMemcpyHostToDevice);
   cudaMemcpy(deviceF, F, m*n*sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceW, W, w*w*sizeof(double), cudaMemcpyHostToDevice);
 
   // CUDA events used for measuring time
   cudaEvent_t start, stop;
@@ -181,20 +203,13 @@ int main(int argc, char* argv[]) {
 
   // Start measuring time and call kernel
   cudaEventRecord(start);
-  nonLocalMeans<<<m,n>>>(deviceP, m, n, w, filtSigma, deviceF);
+  nonLocalMeans<<<(m*n/256),256>>>(deviceX, m, n, w, filtSigma, deviceF, deviceW);
   cudaEventRecord(stop);
 
   // Copy data for input and output
   // from CPU memory to GPU memory
-  cudaMemcpy(P, deviceP, m*n*w*w*sizeof(double), cudaMemcpyDeviceToHost);
+  cudaMemcpy(X, deviceX, (m+w-1)*(n+w-1)*sizeof(double), cudaMemcpyDeviceToHost);
   cudaMemcpy(F, deviceF, m*n*sizeof(double), cudaMemcpyDeviceToHost);
-
-  // Find original denoised pixel
-  // divinding by center pixel
-  // of gaussian kernel value
-  for(int i = 0; i < m; ++i)
-    for(int j = 0; j < n; ++j)
-      F[i*n+j] /= W[(w-1)/2*w+(w-1)/2];
 
   cudaEventSynchronize(stop);
   float milliseconds = 0;
@@ -209,23 +224,23 @@ int main(int argc, char* argv[]) {
 
   // Write filtered image to csv txt file
   // used by matlab script
-  sprintf(outputFileName, "../output_images/output_images_csv_txt/output_images_V1/%s_%d_%d_denoised.txt", argv[7], n, w);
+  sprintf(outputFileName, "../output_images/output_images_csv_txt/output_images_V2/%s_%d_%d_denoised.txt", argv[7], n, w);
   // printf("Writing denoised image to %s\n", outputFileName);
   printMatrixCsv(F, m, n, outputFileName);
 
-  sprintf(outputFileName, "../output_images/output_images_csv_txt/output_images_V1/%s_%d_%d_residual.txt", argv[7], n, w);
+  sprintf(outputFileName, "../output_images/output_images_csv_txt/output_images_V2/%s_%d_%d_residual.txt", argv[7], n, w);
   // printf("Writing residual image to %s\n", outputFileName);
   printMatrixCsv(R, m, n, outputFileName);
 
   printf("%lf\n", milliseconds);
 
   // Deallocate CPU and GPU memory
-  cudaFree(deviceP);
+  cudaFree(deviceX);
   cudaFree(deviceF);
+  cudaFree(deviceW);
   free(X);
   free(F);
   free(W);
-  free(P);
   free(R);
 
   return 0;
